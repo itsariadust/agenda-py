d26529c4633da6b7a0a52e50c3a5532f8bb64564
itsariadust <jakobaindreas11@gmail.com>
2024-12-13 21:14:24 +0800

    Added some kind of input validation

--- add_event_ui.py
+++ add_event_ui.py
@@ -1,24 +1,38 @@
 import ttkbootstrap as ttk
 from ttkbootstrap.widgets import Entry, Checkbutton, DateEntry
 from tkinter.simpledialog import Dialog
-
+from datetime import datetime
 
 class AddEventDialog(Dialog):
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
 
+    def validate_time(self, x) -> bool:
+        if self.strptime_test(x, "%H:%M"):
+            return True
+        elif x == "":
+            return False
+        else:
+            return False
+
+    def strptime_test(self, date, datetime_format):
+        try:
+            datetime.strptime(date, datetime_format)
+            return True
+        except ValueError:
+            return False
+
     def body(self, master):
-        # Event Name
+        time_func = self.register(self.validate_time)
+
         ttk.Label(master, text="Event Name:").grid(row=0, column=0, sticky=ttk.W)
         self.event_name = Entry(master)
         self.event_name.grid(row=0, column=1, padx=10, pady=5)
 
-        # Description
         ttk.Label(master, text="Description:").grid(row=1, column=0, sticky=ttk.W)
         self.description = Entry(master)
         self.description.grid(row=1, column=1, padx=10, pady=5)
 
-        # All Day Event
         ttk.Label(master, text="All Day Event:").grid(row=2, column=0, sticky=ttk.W)
         self.is_all_day = ttk.BooleanVar(value=True)
         self.all_day_check = Checkbutton(
@@ -26,30 +40,25 @@ class AddEventDialog(Dialog):
         )
         self.all_day_check.grid(row=2, column=1, padx=10, pady=5)
 
-        # Start Date
         ttk.Label(master, text="Start Date (MM-DD-YYYY):").grid(row=3, column=0, sticky=ttk.W)
         self.start_date = DateEntry(master, dateformat="%m-%d-%Y")
         self.start_date.grid(row=3, column=1, padx=10, pady=5)
 
-        # End Date
         ttk.Label(master, text="End Date (MM-DD-YYYY):").grid(row=4, column=0, sticky=ttk.W)
         self.end_date = DateEntry(master, dateformat="%m-%d-%Y")
         self.end_date.grid(row=4, column=1, padx=10, pady=5)
 
-        # Start Time (disabled if all-day event is checked)
         ttk.Label(master, text="Start Time (in 24-hour format):").grid(row=5, column=0, sticky=ttk.W)
-        self.start_time = Entry(master)
+        self.start_time = Entry(master, validate="focus", validatecommand=(time_func, '%P'))
         self.start_time.grid(row=5, column=1, padx=10, pady=5)
         self.start_time.config(state="disabled")
 
-        # End Time (disabled if all-day event is checked)
         ttk.Label(master, text="End Time (in 24-hour format):").grid(row=6, column=0, sticky=ttk.W)
-        self.end_time = Entry(master)
+        self.end_time = Entry(master, validate="focus", validatecommand=(time_func, '%P'))
         self.end_time.grid(row=6, column=1, padx=10, pady=5)
         self.end_time.config(state="disabled")
 
     def toggle_time_fields(self):
-        """Toggle the state of the start and end time fields based on the all-day event checkbutton."""
         if self.is_all_day.get():
             self.start_time.config(state="disabled")
             self.end_time.config(state="disabled")
--- edit_event_ui.py
+++ edit_event_ui.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import ttkbootstrap as ttk
 from ttkbootstrap.widgets import Entry, Checkbutton, DateEntry
 from tkinter.simpledialog import Dialog
@@ -8,20 +10,34 @@ class EditEventDialog(Dialog):
         self.event = event
         super().__init__(parent, **kwargs)
 
+    def validate_time(self, x) -> bool:
+        if self.strptime_test(x, "%H:%M"):
+            return True
+        elif x == "":
+            return False
+        else:
+            return False
+
+    def strptime_test(self, date, datetime_format):
+        try:
+            datetime.strptime(date, datetime_format)
+            return True
+        except ValueError:
+            return False
+
     def body(self, master):
-        # Event Name
+        time_func = self.register(self.validate_time)
+
         ttk.Label(master, text="Event Name:").grid(row=0, column=0, sticky=ttk.W)
         self.event_name = Entry(master)
         self.event_name.grid(row=0, column=1, padx=10, pady=5)
         self.event_name.insert(0, self.event.name)
 
-        # Description
         ttk.Label(master, text="Description:").grid(row=1, column=0, sticky=ttk.W)
         self.description = Entry(master)
         self.description.grid(row=1, column=1, padx=10, pady=5)
         self.description.insert(0, self.event.description)
 
-        # All Day Event
         ttk.Label(master, text="All Day Event:").grid(row=2, column=0, sticky=ttk.W)
         self.is_all_day = ttk.BooleanVar(value=self.event.all_day)
         self.all_day_check = Checkbutton(
@@ -29,28 +45,24 @@ class EditEventDialog(Dialog):
         )
         self.all_day_check.grid(row=2, column=1, padx=10, pady=5)
 
-        # Start Date
         ttk.Label(master, text="Start Date:").grid(row=3, column=0, sticky=ttk.W)
         self.start_date = DateEntry(master, dateformat="%m-%d-%Y", startdate=self.event.start_date)
         self.start_date.grid(row=3, column=1, padx=10, pady=5)
 
-        # End Date
         ttk.Label(master, text="End Date:").grid(row=4, column=0, sticky=ttk.W)
         self.end_date = DateEntry(master, dateformat="%m-%d-%Y", startdate=self.event.end_date)
         self.end_date.grid(row=4, column=1, padx=10, pady=5)
 
-        # Start Time (disabled if all-day event is checked)
         ttk.Label(master, text="Start Time:").grid(row=5, column=0, sticky=ttk.W)
-        self.start_time = Entry(master)
+        self.start_time = Entry(master, validate="focus", validatecommand=(time_func, '%P'))
         self.start_time.grid(row=5, column=1, padx=10, pady=5)
         if not self.event.all_day:
             self.start_time.insert(0, self.event.start_time)
         else:
             self.start_time.config(state="disabled")
 
-        # End Time (disabled if all-day event is checked)
         ttk.Label(master, text="End Time:").grid(row=6, column=0, sticky=ttk.W)
-        self.end_time = Entry(master)
+        self.end_time = Entry(master, validate="focus", validatecommand=(time_func, '%P'))
         self.end_time.grid(row=6, column=1, padx=10, pady=5)
         if not self.event.all_day:
             self.end_time.insert(0, self.event.end_time)
