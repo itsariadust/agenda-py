563943b50d39c22ff063715f15637dea93499461
itsariadust <jakobaindreas11@gmail.com>
2024-12-13 19:39:56 +0800

    Added function where the table shows events that share the same starting date

--- ui.py
+++ ui.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import ttkbootstrap as ttk
 from ttkbootstrap.tableview import Tableview
 from ttkbootstrap.constants import *
@@ -15,8 +17,10 @@ from event_create import EventCreate
 class AgendaUI(ttk.Frame):
     def __init__(self, master_window):
         super().__init__(master_window)
+        self.date_var = None
         self.agenda = Agenda()
         self.selected_row = None
+        self.selected_date = None
         self.pack(fill=BOTH, expand=YES)
         self.sidebar()
         self.table = self.create_table()
@@ -36,25 +40,24 @@ class AgendaUI(ttk.Frame):
             master=self,
             coldata=col_data,
             rowdata=[],
-            paginated=True,
-            pagesize=10,
-            autofit=True,
+            autofit=False,
             bootstyle=PRIMARY,
         )
 
         table.pack(fill=BOTH, expand=YES, padx=10, pady=10)
-        table.view.bind('<ButtonRelease-1>', self.on_row_select)
+        table.view.bind('<ButtonRelease-1>', lambda e : self.on_row_select())
         return table
 
-    def on_row_select(self, event):
-        selected_item = self.table.view.selection()[0]
-        item_data = self.table.view.item(selected_item)
-        self.selected_row = item_data["values"][0]
+    def on_row_select(self):
+        selected_row = self.table.view.selection()[0]
+        selected_row_data = self.table.view.item(selected_row)
+        self.selected_row = selected_row_data["values"][0]
 
     def refresh_table(self):
         self.table.delete_rows()
-        for event_date, events in self.agenda.events.items():
-            for event_id, event_obj in events.items():
+        if self.selected_date:
+            selected_date_events = self.agenda.events.get(self.selected_date)
+            for event_id, event_obj in selected_date_events.items():
                 row = (
                     event_id,
                     event_obj.name,
@@ -63,7 +66,19 @@ class AgendaUI(ttk.Frame):
                     f"{event_obj.start_date.strftime('%m-%d-%Y')} ({getattr(event_obj, 'start_time', '00:00')})",
                     f"{event_obj.end_date.strftime('%m-%d-%Y')} ({getattr(event_obj, 'end_time', '00:00')})",
                 )
-                self.table.insert_row('end', row)
+                self.table.insert_row(values=row)
+        else:
+            for event_date, events in self.agenda.events.items():
+                for event_id, event_obj in events.items():
+                    row = (
+                        event_id,
+                        event_obj.name,
+                        event_obj.description,
+                        event_obj.all_day,
+                        f"{event_obj.start_date.strftime('%m-%d-%Y')} ({getattr(event_obj, 'start_time', '00:00')})",
+                        f"{event_obj.end_date.strftime('%m-%d-%Y')} ({getattr(event_obj, 'end_time', '00:00')})",
+                    )
+                    self.table.insert_row(values=row)
         self.table.load_table_data()
         self.agenda.rebuild_index()
         self.agenda.save_agenda()
@@ -72,10 +87,12 @@ class AgendaUI(ttk.Frame):
         container = ttk.Frame(self)
         container.pack(side=LEFT, anchor=N, padx=10, pady=10, fill=Y)
 
-        date = DateEntry(
+        self.date_var = ttk.StringVar()
+        date_entry = DateEntry(
             master=container,
         )
-        date.pack(padx=5, pady=5, fill=X, expand=YES)
+        date_entry.pack(padx=5, pady=5, fill=X, expand=YES)
+        date_entry.bind("<FocusOut>", lambda e: self.update_selected_date(date_entry))
 
         add_btn = ttk.Button(
             master=container,
@@ -102,6 +119,23 @@ class AgendaUI(ttk.Frame):
         )
         cancel_btn.pack(padx=5, pady=5, fill=X)
 
+        show_all_btn = ttk.Button(
+            master=container,
+            text="Show All",
+            width=6,
+            command=self.show_all_events,
+        )
+        show_all_btn.pack(padx=5, pady=5, fill=X)
+
+    def show_all_events(self):
+        self.selected_date = None
+        self.refresh_table()
+
+    def update_selected_date(self, date_entry):
+        new_date = datetime.strptime(date_entry.entry.get(), "%m/%d/%Y").date()
+        self.selected_date = new_date
+        self.refresh_table()
+
     def add_event(self):
         dialog = AddEventDialog(self, title="New Event")
 
@@ -125,7 +159,6 @@ class AgendaUI(ttk.Frame):
     def delete_event(self):
         event_to_edit = self.selected_row
         event_data = self.agenda.event_index[event_to_edit][1]
-        print(event_data.id)
         dialog = DeleteEventDialog(self, title="Delete Event", event=event_data)
 
         if dialog.result:
@@ -137,6 +170,6 @@ class AgendaUI(ttk.Frame):
 if __name__ == "__main__":
     app = ttk.Window(title="Agenda",
                      themename="darkly",
-                     size=(800,600))
+                     size=(1200,900))
     AgendaUI(app)
     app.mainloop()
